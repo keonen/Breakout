5d452ac77544736f16837d985eb05fef5ca485a2
keonen <keijo.olavi.kangas@gmail.com>
2015-08-04 12:01:22 +0300

    2015-08-04

--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -3,15 +3,19 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
+	Animator animator;
+
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		Destroy (gameObject, 0.01f);
+		animator.SetTrigger("ChangeState");
+		GetComponent<BoxCollider2D>().enabled = false;
+		Destroy (gameObject, 1f);
 	}
 	
 	// Use this for initialization
 	void Start () 
 	{
-
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
