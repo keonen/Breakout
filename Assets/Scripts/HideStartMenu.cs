0a0fd731f821a2bd7502578674ea6a9e746446dc
keonen <keijo.olavi.kangas@gmail.com>
2015-08-04 15:01:38 +0300

    2015-08-04 15:01

---
+++ Assets/Scripts/HideStartMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideStartMenu : MonoBehaviour {
+
+	public GameObject parentObject;
+
+	// Use this for initialization
+	void Start () {
+		parentObject=GameObject.Find("MainMenu");
+
+		foreach(Renderer r in parentObject.GetComponentsInChildren<Renderer>())
+			r.enabled=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
